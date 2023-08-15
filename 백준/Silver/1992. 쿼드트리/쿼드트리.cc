94994d26cc8c8b37862f0f4cf7fbac80577fbead
DoHeunKo <98294597+DoHeunKo@users.noreply.github.com>
2023-08-15 16:27:12 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,35 +1,36 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<string>
 
 using namespace std;
-
 const int max_n = 70;
-int n;
-string str;
-int a[max_n][max_n];
 
-string recur(int y, int x, int _sz) {
-    if (_sz == 1) {
-        return string(1, a[y][x]);
-    }
-    char cur = a[y][x];
-    string temp = "";
+char a[max_n][max_n];
+int t;
+string str;
+string result;
+
+string go(int y, int x, int _sz) {
+    
+    if (_sz == 1) return string(1, a[y][x]);
+    
+    char temp = a[y][x];
     for (int i = y; i < y + _sz; i++) {
         for (int j = x; j < x + _sz; j++) {
-            if (cur != a[i][j]) {
-                temp += '(';
-                temp += recur(y, x, _sz / 2);
-                temp += recur(y, x + _sz / 2, _sz / 2);
-                temp += recur(y + _sz/2, x, _sz / 2);
-                temp += recur(y + _sz/2, x + _sz/2, _sz / 2);
-                temp += ')';
-                return temp;
+            string ret = "";
+            if (temp != a[i][j]) {
+                ret += '(';
+                ret += go(y, x, _sz / 2);
+                ret += go(y, x + _sz / 2, _sz / 2);
+                ret += go(y + _sz / 2, x, _sz / 2);
+                ret += go(y + _sz / 2, x + _sz / 2, _sz / 2);
+                ret += ')';
+                return ret;
             }
         }
     }
     return string(1, a[y][x]);
+
 }
 
 int main() {
@@ -38,15 +39,17 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> n;
-    for (int i = 0; i < n; i++) {
+    cin >> t;
+    for (int i = 0; i < t; i++) {
         cin >> str;
         for (int j = 0; j < str.size(); j++) {
             a[i][j] = str[j];
         }
     }
+    
+    result=go(0, 0, t);
 
-    string result=recur(0, 0, n);
     cout << result << '\n';
+
     return 0;
 }
